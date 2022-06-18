9716c34ab9bfe34d2fbc81e725503456e52dcdfe
HZ <hz.impact@outlook.com>
2022-06-18 11:10:18 +0800

    update: test and appbar

--- app/lib/views/app.dart
+++ app/lib/views/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:app/widgets/searchbar.dart';
+// import 'package:app/widgets/searchbar.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -15,10 +15,35 @@ class _AppState extends State<App> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dev Eng'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('TODO: Search')),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+          ),
+        ],
       ),
-      body: Flex(
-        direction: Axis.vertical,
-        children: const <Widget>[Text('a'), Text('b')],
+      body: ListView(
+        children: <Widget>[
+          Row(
+            children: const [
+              Text("单词"),
+              Text("翻译"),
+              Text("发音"),
+              Text("例句"),
+              Text("备注"),
+            ],
+          )
+        ],
       ),
     );
   }
